5e58c8d3d04904a3977df1f4ff056d387eb9dd03
Debora Santos <deboramozart.santos@gmail.com>
2024-11-09 22:52:11 -0300

    feat: adding sound efects

---
+++ src/script/main.js
@@ -0,0 +1,21 @@
+function playSound(audioName){
+    let audio = new Audio(`/src/sounds/${audioName}.mp3`);
+    audio.volume = 0.09
+    audio.play()
+}
+
+function typingSound(){
+    playSound("writing-sound");
+}
+
+function btnConfirm(){
+    playSound("confirm-sound");
+}
+
+function btnPlus(){
+    playSound("hit-sound-xp");
+}
+
+function btnSub(){
+    playSound("hit-sound-xp");
+}
